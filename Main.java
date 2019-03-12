c95209fb2fdbe8735cfe58ff2b087e1ed03d98ef
KenzaburoTakagi <ken.takagi216@gmail.com>
2019-03-13 00:05:27 +0900

    4-4

--- Main.java
+++ Main.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args){
-    boolean boolA = true;
-    int intNum = boolA;
+    int[] numbers = {3, 4, 5};
+    System.out.println("1桁の数字を入力してください");
+    int input = new java.util.Scanner(System.in).nextInt();
+    for (int n : numbers) {
+      if (n == input) {
+        System.out.println("あたり!");
+      }
+    }
   }
 }
