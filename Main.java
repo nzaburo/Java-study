65def4789096a0ae0eabb5c80f868a18ff5b6ceb
KenzaburoTakagi <ken.takagi216@gmail.com>
2019-03-13 15:06:08 +0900

    5-7

--- Main.java
+++ Main.java
@@ -2,13 +2,12 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args){
-    int[] numbers = {3, 4, 5};
-    System.out.println("1桁の数字を入力してください");
-    int input = new java.util.Scanner(System.in).nextInt();
-    for (int n : numbers) {
-      if (n == input) {
-        System.out.println("あたり!");
-      }
-    }
+    int ans = add(100, 10);
+    System.out.println("100 + 10 =" + ans);
+  }
+
+  public static int add(int x, int y){
+    int ans = x + y;
+    return ans;
   }
 }
